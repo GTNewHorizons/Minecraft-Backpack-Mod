e976faff2d439559a2dd1547f36b19a58fe1ac51
chill <chill.gtnh@outlook.com>
2023-05-12 14:14:12 +0200

    collapse identical catch statements
    FileNotFoundException extends IOException.
    Both catches were identical otherwise.

--- src/main/java/de/eydamos/backpack/handler/SaveFileHandler.java
+++ src/main/java/de/eydamos/backpack/handler/SaveFileHandler.java
@@ -2,7 +2,6 @@ package de.eydamos.backpack.handler;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -122,11 +121,8 @@ public class SaveFileHandler {
             if (fileNew.exists()) {
                 fileNew.delete();
             }
-        } catch (FileNotFoundException fileNotFoundException) {
+        } catch (IOException fileNotFoundException) {
             fileNotFoundException.printStackTrace();
-            logger.warn("[Backpack] Couldn't save data.");
-        } catch (IOException ioException) {
-            ioException.printStackTrace();
             logger.warn("[Backpack] Couldn't save data.");
         }
     }
