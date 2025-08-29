e61fdcef9dfa3515c5b27f400a9ec8afec727a95
Wilhelm Schuster <dev@rot13.io>
2025-08-29 14:54:32 +0200

    Fix typo in mod name

--- src/main/java/de/eydamos/backpack/misc/Constants.java
+++ src/main/java/de/eydamos/backpack/misc/Constants.java
@@ -5,7 +5,7 @@ import net.minecraft.util.ResourceLocation;
 public class Constants {
 
     public static final String MOD_ID = "Backpack";
-    public static final String MOD_NAME = "Backpack Editted for ModdedNetwork";
+    public static final String MOD_NAME = "Backpack Edited for ModdedNetwork";
     public static final String MOD_VERSION = "GRADLETOKEN_VERSION";
 
     public static final String DOMAIN = "backpack";
