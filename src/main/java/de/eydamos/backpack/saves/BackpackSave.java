524937800fbe8f7c9a15ba594042d63a52f4bc46
Algent <algent@gmail.com>
2026-07-01 10:05:55 +0200

    Fix backpack save metadata desync on open

--- src/main/java/de/eydamos/backpack/saves/BackpackSave.java
+++ src/main/java/de/eydamos/backpack/saves/BackpackSave.java
@@ -39,7 +39,7 @@ public class BackpackSave extends AbstractSave {
         } else {
             if (backpack.getItem() instanceof ItemBackpackBase) {
                 load(NBTItemStackUtil.getString(backpack, Constants.NBT.UID));
-                if (force || getType() == 0 || getSize() <= 0) {
+                if (force || getType() != BackpackUtil.getType(backpack) || getSize() != getSize(backpack)) {
                     initialize(backpack);
                 }
             }
